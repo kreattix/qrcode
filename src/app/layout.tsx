f2fe1cd27a64aaece6e9bf789aeb2bbce228a56a
Prince Patel <prpatel0949@gmail.com>
2025-03-11 21:53:16 +0530

    update analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -73,6 +74,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
+        <GoogleAnalytics />
         {children}
         <Toaster position="bottom-center" />
       </body>
